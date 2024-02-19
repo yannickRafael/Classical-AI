093bbc83e43607f549df10b79a4c0299e686abdc
Yannick Rafael <yannickrafael286@gmail.com>
2024-02-19 07:11:37 +0200

    first commit

---
+++ Question 2/notebook.py
@@ -0,0 +1,42 @@
+from IPython.display import HTML
+from IPython.display import display
+from inspect import getsource
+
+# import time
+# from collections import defaultdict
+# import ipywidgets as widgets
+# import matplotlib.pyplot as plt
+# import networkx as nx
+# import numpy as np
+# from PIL import Image
+# from matplotlib import lines
+
+
+
+def pseudocode(algorithm):
+    """Print the pseudocode for the given algorithm."""
+    from urllib.request import urlopen
+    from IPython.display import Markdown
+
+    algorithm = algorithm.replace(' ', '-')
+    url = "https://raw.githubusercontent.com/aimacode/aima-pseudocode/master/md/{}.md".format(algorithm)
+    f = urlopen(url)
+    md = f.read().decode('utf-8')
+    md = md.split('\n', 1)[-1].strip()
+    md = '#' + md
+    return Markdown(md)
+
+
+def psource(*functions):
+    """Print the source code for the given function(s)."""
+    source_code = '\n\n'.join(getsource(fn) for fn in functions)
+    try:
+        from pygments.formatters import HtmlFormatter
+        from pygments.lexers import PythonLexer
+        from pygments import highlight
+
+        display(HTML(highlight(source_code, PythonLexer(), HtmlFormatter(full=True))))
+
+    except ImportError:
+        print(source_code)
+
